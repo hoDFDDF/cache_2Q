57a2c24cacd6dcf9339f64f09ed9b5fd980dbdb6
mr. haizenberg <iefejj23@gmail.com>
2026-05-03 17:01:26 +0300

    don't build

---
+++ build/CMakeFiles/Tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tests.dir/Tests/unit_tests/unit_tests.cpp.o"
+  "CMakeFiles/Tests.dir/Tests/unit_tests/unit_tests.cpp.o.d"
+  "libTests.a"
+  "libTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/modules.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modules.dir/modules/source/2Q_cache.cpp.o"
+  "CMakeFiles/modules.dir/modules/source/2Q_cache.cpp.o.d"
+  "libmodules.a"
+  "libmodules.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/modules.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
