4136c225e340f2a249f5b477db8228b34d68ec33
pasha <lana.pchelket@gmail.com>
2026-09-21 00:15:06 +0300

    raw version LFU cache

--- modules/include/LFU_cache.hpp
+++ modules/include/LFU_cache.hpp
@@ -1,51 +1,115 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-
+#include <list>
 
 namespace cache {
 
     template <typename keyT, typename Value>
 
     class cache_LFU_ {
-        size_t capasity_;
-        size_t min_freq_;
-
+        std::size_t capacity_;
+        
         struct FrequencyGroup {
             std::size_t frequency = 0;
             std::list<keyT> keys;
         };
+
+        std::list<FrequencyGroup> Fgroups_;
         
+        struct Entry {
+            Value value;
+            typename FrequencyGroupsLst::iterator group_it;
+            typename std::list<KeyT>::iterator key_it;
+        };
 
-        std::unordered_map<keyT, Value> entries;
-        std::unordered_map<Freq
+        std::unordered_map<keyT, Entry> entries_;
+
+        // is there element in cache or not?
         bool get(keyT key) {
+            if (entries_.contains(key)) return true;
 
+            return false;
         }
  
-        bool put(keyT key, Value value) {
+        // add new element to cache
+        void put(keyT key, Value value) {
+            if (Fgroups_.empty() || Fgroups_.front().frequency != 1) {
+                Fgroups_.push_front(FrequencyGroup{1, {}});
+            }
+                auto group_it = Fgroups_.begin();
+                group_it->keys.push_front(key);
 
+                auto key_it = group_it->keys.begin();
+
+                entries_.emplace(key, Entry{value, group_it, key_it});
         }        
 
-        void touch()
+        // increase frequency existing element
+        void touch(Entry& entry) {
+            auto cur_group_it = entry.group_it;
+            next_group_it = ++cur_group_it;
+            new_frequency = ++entry.group_it->frequency;
 
+            if (next_group_it == Fgroups_.end() ||
+                next_group_it->frequency != new_frequency) {
+
+                    next_group_it = Fgroups_.insert(next_group_it,
+                                                   FrequencyGroup{new_frequency, {}});
+            }
+            
+            next_group_it->keys.splice(next_group_it->keys.begin(),
+                                       cur_group_it->keys, entry.key_it);
+
+            entry.group_it = next_group_it;
+
+            if (cur_group_it->keys.empty()) {
+                Fgroups_.erase(cur_group_it);
+            }
+        }
+
+        // erase one element (least frequency and least used)
+        void evictOne() {
+            auto first_group_it  = Fgroups_.begin();
+
+            entries_.erase(first_group_it->keys.back());
+            first_group_it->keys.pop_back();
+
+            if (first_group_it->keys.empty()) {
+                Fgroups_.erase(first_group_it);
+            }
+        }
 
 
         public:
         
-            explicit cache_LFU_(std::size_t capasity) : capasity_{capacity} {
-        
+            // constructor
+            explicit cache_LFU_(std::size_t capacity)
+                : capacity_{capacity} {}
+
             template <typename F> 
 
-            bool lookUpUpdate(keyT key, F slow_get_page) {
+            // the main public function. True if element is in cache, false against
+            bool lookUpUpdate(const keyT& key, F slow_get_page) {
+                if (capasity_ == 0) return false;
+
+                if (get(key)) {
+                    touch(entries_.find(key));
+                    return true;
+                }
                 
+                if (entries_.size() == capacity_)
+                    evictOne();
+                put(key, slow_get_page(key));
+               
+                return false;
             }
 
 
-        }
-    }
+    };
 
 
 
--- modules/source/LFU_cache.cpp
+++ modules/source/LFU_cache.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <cassert>
 
+#include "LFU_cache.hpp"
 
 
 
 
-
+int slow_get_page(int key) { return key; }
 
 int main () {
+    int hits = 0; 
+    size_t cache_size     = 0;
+    size_t cache_capacity = 0;
+    
+    assert(std::cin.good());
 
+    std::cin >> cache_size >> cache_capacity;
+    cache::cache_LFU_<int, int> cache_1{cache_capacity};
 
+    for (int i = 0; i < cache_size; i++) {
+        int key;
 
-
-
-
-
-
-
-
-
+        assert(std::cin.good());
+        std::cin >> key;
+        if (cache_1.lookUpUpdate(key, slow_get_page)) {
+            hits++;
+        }
+    }
+    std::cout << hits;
 
     return 0;
 }
